21aece17b193c7d2fed123945439b879ed9e4b26
Joyless <65855333+Joy-less@users.noreply.github.com>
2026-04-07 21:12:15 +0100

    Improve AsStringName/AsNodePath

--- GodotStringIntercept/GodotStringInterceptExtensions.cs
+++ GodotStringIntercept/GodotStringInterceptExtensions.cs
@@ -7,33 +7,37 @@ namespace GodotStringIntercept;
 /// </summary>
 public static class GodotStringInterceptExtensions {
     /// <summary>
-    /// Returns a <see cref="StringName"/> created from the given string.
-    /// <para>
+    /// Returns a <see cref="StringName"/> matching the given string.
+    /// <list type="bullet">
+    /// <item>
     /// When called with a <b>compile-time constant</b>, the source generator
     /// intercepts this call and replaces it with a direct field read.
-    /// </para>
-    /// <para>
+    /// </item>
+    /// <item>
     /// When called with a <b>non-constant</b>, the source generator cannot intercept
     /// the call; a new instance is constructed on every call.
-    /// </para>
+    /// </item>
+    /// </list>
     /// </summary>
     public static StringName AsStringName(this string str) {
-        GD.PushWarning($"The call to {nameof(AsStringName)} was not intercepted. A new {nameof(StringName)} will be allocated.");
+        GD.PushWarning($"The call to {nameof(AsStringName)} with '{str}' was not intercepted. Allocating a new {nameof(StringName)}.");
         return new StringName(str);
     }
     /// <summary>
-    /// Returns a <see cref="NodePath"/> created from the given string.
-    /// <para>
+    /// Returns a <see cref="NodePath"/> matching the given string.
+    /// <list type="bullet">
+    /// <item>
     /// When called with a <b>compile-time constant</b>, the source generator
     /// intercepts this call and replaces it with a direct field read.
-    /// </para>
-    /// <para>
+    /// </item>
+    /// <item>
     /// When called with a <b>non-constant</b>, the source generator cannot intercept
     /// the call; a new instance is constructed on every call.
-    /// </para>
+    /// </item>
+    /// </list>
     /// </summary>
     public static NodePath AsNodePath(this string str) {
-        GD.PushWarning($"The call to {nameof(AsNodePath)} was not intercepted. A new {nameof(NodePath)} will be allocated.");
+        GD.PushWarning($"The call to {nameof(AsNodePath)} with '{str}' was not intercepted. Allocating a new {nameof(NodePath)}.");
         return new NodePath(str);
     }
 }
